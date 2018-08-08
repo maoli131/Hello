8ef66cd5981282b533e399138eff635dbf1312a6
Mao <maoli1998@outlook.com>
2018-08-08 21:22:53 +0100

    First HelloWorld Commitment.

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/** A sample Java program HelloWorld
+ * @Author Mao Li
+ * @Date Aug 8th, 2018
+ */
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
