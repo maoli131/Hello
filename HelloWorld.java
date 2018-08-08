ed44132732bb268abdb10477a7c49a8f4c3c399b
Mao <maoli1998@outlook.com>
2018-08-08 22:24:08 +0100

    Print more lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,12 @@ public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World!");
         System.out.println("A new line added");
+        loopPrint();
     }
 
+    // This prints out many times of I Love You
+    public static void loopPrint () {
+        for (int i = 0; i < 10; i++)
+            System.out.println("I love you");
+    }
 }
