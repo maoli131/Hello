4ea49ae3077c6de8f0680a13ec52c976328989ac
Mao <maoli1998@outlook.com>
2018-10-03 12:10:49 -0700

    Added python and cpp Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,13 +7,18 @@ public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World!");
         System.out.println("I'd like to say");
-        loopPrint();
+        testPrintC(20.22);
     }
 
     // This prints out many times of I Love You
-    public static void loopPrint () {
-        int times = 5;
+    public static void loopPrint (int times, String message) {
         for (int i = 0; i < times; i++)
-            System.out.println("I love you");
+            System.out.println(message);
     }
+
+    // This tests C style print statement
+    public static void testPrintC (double average) {
+        System.out.printf("Wha is average? %f \n", average);
+    }
+
 }
